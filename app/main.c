f6b6b77d187c1f881af29ef4bee44579800a54de
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-02 15:25:15 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -4,10 +4,8 @@
 
 int main() {
   char input[100];
-  char check_for_com[10];
   while (1){
     int i = 0;
-    // Uncomment this block to pass the first stage
     printf("$ ");
     // Flush after every printf
     setbuf(stdout, NULL);
@@ -17,12 +15,8 @@ int main() {
     input[strlen(input) - 1] = '\0';
     if (strcmp(input, "exit 0") == 0)
       exit(0);
-    for (i = 0; input[0] == 'e' && input[i] != ' '; i++){
-      check_for_com[i] = input[i];
-    }
-    check_for_com[i] = '\0';
-    if (strcmp(check_for_com, "echo") == 0)
-      printf("%s\n", &input[i]);
+    if (strncmp(input, "echo ", 5) == 0) 
+      printf("%s\n", &input[5]);
     else
       printf("%s: command not found\n", input);
   }
