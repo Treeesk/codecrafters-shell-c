2796aeba68abb9ae213226b9d2e89470dd540573
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-05 18:00:15 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -20,27 +20,37 @@ void print_without_spaces(const char *inp){
   }
 }
 
-void parse_input(char *inp, char **argv, int *argc){
-  // char *start = inp, *end = inp;
-  // short int in_quotes = 0;
-  // for (int i = 0; inp[i]; i++){
-  //   if ((inp[i] == '\'' || inp[i] == '\"') && in_quotes == 0){
-  //     in_quotes = 1;
-  //     start = &inp[i + 1];
-  //   }
-  //   else if ((inp[i] == '\'' || inp[i] == '\"') && in_quotes == 1){
-  //     in_quotes = 0;
-  //     inp[i] = '\0';
-  //     argv[(*argc)++] = start;
-  //     start = NULL;
-  //   }
-    char *token = strtok(inp, " ");
-    while (token != NULL && *argc < 10) {
-        argv[(*argc)++] = token;
-        token = strtok(NULL, " ");
+void parse_input(char *inp, char **argv, int *argc) {
+    char *start = inp;
+    short int in_quotes = 0;
+    
+    while (*start) {
+        if ((*start == '\'' || *start == '\"') && in_quotes == 0) {
+            in_quotes = 1;
+            start++;
+        } else if ((*start == '\'' || *start == '\"') && in_quotes == 1) {
+            in_quotes = 0;
+            *start = '\0'; // Завершить текущий токен
+            argv[(*argc)++] = start + 1; // Сохранить токен
+        } else if (!in_quotes && *start == ' ') {
+            *start = '\0'; // Завершить токен
+            argv[(*argc)++] = start + 1; // Сохранить токен
+        }
+        start++;
     }
-    argv[*argc] = NULL;
+    argv[*argc] = NULL; // Завершить массив аргументов
 }
+  // while (token && argc < 10){
+  //   if (token[0] == '\'' || token[0] == '\"'){
+  //     size_t len = strlen(token);
+  //     if (token[len - 1] == '\'' || token[len - 1] == '\"'){
+  //       token[len - 1] = '\0';
+  //       token++;
+  //     }
+  //   }
+  //   argv[argc++] = token;
+  //   token = strtok(NULL, " ");
+  // }
 
 
 void fork_func(char *full_path, char **argv){
