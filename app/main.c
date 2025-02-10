d7a1d3a1a0b57dbf2d2b2167dfadf19c3034aa7c
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-10 20:02:52 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -111,53 +111,44 @@ int main() {
     if (strcmp(input, "exit 0") == 0)
       exit(0);
     else if (strncmp(input, "echo ", 5) == 0){
-      //   int i = 5;
-      //   short int cnt_space = 0;
-      //   char type_quotes = 0;
-      //   while (input[i]){
-      //     if (input[i] == '\''){
-      //       i++;
-      //       while (input[i] != '\'')
-      //         printf("%c", input[i++]);
-      //     }
-      //     else if (input[i] == '\"'){
-      //       type_quotes = input[i];
-      //       i++;
-      //       while (input[i] && input[i] != type_quotes){
-      //         if (input[i] == '\\')
-      //           if (input[i + 1] == '$' || input[i + 1] == '\'' || input[i + 1] == '\"' || input[i + 1] == '\\')
-      //             i++;
-      //         printf("%c", input[i++]);
-      //       }
-      //       cnt_space = 0;
-      //       type_quotes = 0;
-      //     }
-      //     else {
-      //       if (input[i] == '\\' && (input[i + 1] == '$' || input[i + 1] == '\'' || input[i + 1] == '\"' || input[i + 1] == ' ')){
-      //         cnt_space = 0;
-      //         i++;
-      //       }
-      //       if (input[i] != ' ' && input[i] != '\\'){
-      //         printf("%c", input[i]);
-      //         cnt_space = 0;
-      //       }
-      //       else if (cnt_space == 0 && input[i] == ' '){
-      //         printf("%c", input[i]);
-      //         cnt_space = 1;
-      //       }
-      //     }
-      //     i++;
-      //   }
-      // printf("\n");
-      char *argv[10];
-      int argc = 0;
-      char *output_file = NULL;
-      parse_input(input, argv, &argc, &output_file);
-      char *pth = check_path(argv[0]); // возвращаю полный путь до команды например cat, а затем применяю эту команду к аргументам argv
-      if (pth != NULL)
-        fork_func(pth, argv, output_file); 
-      else 
-        printf("%s: command not found\n", argv[0]);
+        int i = 5;
+        short int cnt_space = 0;
+        char type_quotes = 0;
+        while (input[i]){
+          if (input[i] == '\''){
+            i++;
+            while (input[i] != '\'')
+              printf("%c", input[i++]);
+          }
+          else if (input[i] == '\"'){
+            type_quotes = input[i];
+            i++;
+            while (input[i] && input[i] != type_quotes){
+              if (input[i] == '\\')
+                if (input[i + 1] == '$' || input[i + 1] == '\'' || input[i + 1] == '\"' || input[i + 1] == '\\')
+                  i++;
+              printf("%c", input[i++]);
+            }
+            cnt_space = 0;
+            type_quotes = 0;
+          }
+          else {
+            if (input[i] == '\\' && (input[i + 1] == '$' || input[i + 1] == '\'' || input[i + 1] == '\"' || input[i + 1] == ' ')){
+              cnt_space = 0;
+              i++;
+            }
+            if (input[i] != ' ' && input[i] != '\\'){
+              printf("%c", input[i]);
+              cnt_space = 0;
+            }
+            else if (cnt_space == 0 && input[i] == ' '){
+              printf("%c", input[i]);
+              cnt_space = 1;
+            }
+          }
+          i++;
+        }
+      printf("\n");
     }
     else if (strncmp(input, "type ", 5) == 0){
       if (strcmp(input, "type type") == 0)
