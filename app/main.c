f01ab0641e307d8276064df723dc1575a0ccab94
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-10 20:56:00 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -10,6 +10,8 @@ void parse_input(char *inp, char **argv, int *argc, char **outf) {
   char *start = inp;
   short int in_quotes = 0;
   char type_quotes = 0;
+  char buffer[100];
+  int ind_buf = 0;
   *outf = NULL; // Инициализируем outf как NULL
 
   for (int i = 0; inp[i]; i++) {
@@ -33,30 +35,42 @@ void parse_input(char *inp, char **argv, int *argc, char **outf) {
 
       if ((inp[i] == '\'' || inp[i] == '\"') && !in_quotes) { // Обработка кавычек
           in_quotes = 1;
-          start = &inp[i + 1]; // Начинаем новый аргумент после кавычки
+//          start = &inp[i + 1]; // Начинаем новый аргумент после кавычки
           type_quotes = inp[i];
       } else if (inp[i] == type_quotes && in_quotes) { // Завершение кавычек
           in_quotes = 0;
+          buffer[ind_buf] = '\0';
           inp[i] = '\0'; // Завершаем текущий аргумент
-          argv[(*argc)++] = start; // Добавляем аргумент в массив
-          start = NULL; // Сбрасываем указатель на начало аргумента
+          argv[(*argc)++] = strdup(buffer); // Добавляем аргумент в массив
+          // start = NULL; // Сбрасываем указатель на начало аргумента
+          ind_buf = 0;
       } else if (inp[i] == ' ' && !in_quotes) { // Обработка пробелов
-          if (start != NULL) {
-              inp[i] = '\0'; // Завершаем текущий аргумент
-              argv[(*argc)++] = start; // Добавляем аргумент в массив
-              start = NULL; // Сбрасываем указатель на начало аргумента
+          if (ind_buf > 0) {
+              // inp[i] = '\0'; // Завершаем текущий аргумент
+              // argv[(*argc)++] = start; // Добавляем аргумент в массив
+              // start = NULL; // Сбрасываем указатель на начало аргумента
+              buffer[ind_buf] = '\0';
+              argv[(*argc)++] = strdup(buffer);
+              ind_buf = 0;
           }
-      } else if (start == NULL) { // Начало нового аргумента
-          start = &inp[i];
-      }
+      // } else if (start == NULL) { // Начало нового аргумента
+      //     start = &inp[i];
+      // }
       else if (in_quotes && inp[i] == '\\'){
-        if (inp[i + 1] == '\\' || inp[i + 1] == '$' || inp[i + 1] == '\'' || inp[i + 1] == '\"')
+        if (inp[i + 1] == '\\' || inp[i + 1] == '$' || inp[i + 1] == '\'' || inp[i + 1] == '\"'){
           i++;
+          buffer[ind_buf] = inp[i];
+        }
       }
+      buffer[ind_buf++] = inp[i];
   }
 
-  if (start != NULL) { // Последний аргумент
-      argv[(*argc)++] = start;
+  // if (start != NULL) { // Последний аргумент
+  //     argv[(*argc)++] = start;
+  // }
+  if (ind_buf > 0){
+    buffer[ind_buf] = '\0';
+    argv[*(argc)++] = strdup(buffer);
   }
 
   argv[*argc] = NULL; // Завершаем массив аргументов NULL
