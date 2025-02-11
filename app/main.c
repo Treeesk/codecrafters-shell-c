f4f38a63200fd8209a48d1a6fddc28c5db32ea96
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-11 16:44:22 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -11,10 +11,12 @@ void parse_input(char *inp, char **argv, int *argc, char **outf) {
   short int in_quotes = 0;
   char type_quotes = 0;
   *outf = NULL; // Инициализируем outf как NULL
+  short int check = 0;
 
   for (int i = 0; inp[i]; i++) {
     if (inp[i] == '\\' && ((type_quotes == '\"' && (inp[i + 1] == '\\' || inp[i + 1] == '\"')) || (type_quotes == '\'' && (inp[i + 1] == '\\' || inp[i + 1] == '\'' || inp[i + 1] == '\"')))){
       memmove(&inp[i], &inp[i + 1], strlen(&inp[i + 1]) + 1); // strlen(inp[i + 1]) + 1 inp[i + 1]-начинаем длину считать со следующего символа, +1 - для \0
+      check = 1;
       continue;
     }
 
@@ -43,7 +45,7 @@ void parse_input(char *inp, char **argv, int *argc, char **outf) {
     else if (inp[i] == type_quotes && in_quotes) { // Завершение кавычек
       if (i > 0 && inp[i - 1] != '\\'){
           in_quotes = 0;
-          inp[i] = '\0'; // Завершаем текущий аргумент
+          inp[check ? i - 1 : i] = '\0'; // Завершаем текущий аргумент
           argv[(*argc)++] = start;
           start = NULL; // Сбрасываем указатель на начало аргумента
       }
