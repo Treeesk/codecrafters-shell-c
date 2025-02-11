2fbac0328503bb483f9630ff39691b7a4c25fb59
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-11 13:31:52 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -15,6 +15,7 @@ void parse_input(char *inp, char **argv, int *argc, char **outf) {
   for (int i = 0; inp[i]; i++) {
     if (inp[i] == '\\' && ((type_quotes == '\"' && (inp[i + 1] == '\\' || inp[i + 1] == '\"')) || (type_quotes == '\'' && (inp[i + 1] == '\\' || inp[i + 1] == '\'' || inp[i + 1] == '\"')))){
       memmove(&inp[i], &inp[i + 1], strlen(&inp[i + 1]) + 1); // strlen(inp[i + 1]) + 1 inp[i + 1]-начинаем длину считать со следующего символа, +1 - для \0
+      i++;
       continue;
     }
 
