58b6f815a2486b8a376759d094badfd847f58bfe
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-13 13:24:46 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -39,11 +39,15 @@ void parse_input(char *inp, char **argv, int *argc, char **outf) {
             type_quotes = inp[i];
         } 
         else if (inp[i] == type_quotes && in_quotes ) { // Завершение кавычек
-          if ((inp[i + 1] == ' ' || inp[i + 1] == '\0') && inp[i + 1] != '\"'){
+          if (inp[i + 1] == ' ' || inp[i + 1] == '\0'){
             in_quotes = 0;
             inp[i] = '\0'; // Завершаем текущий аргумент
             argv[(*argc)++] = start;
             start = NULL; // Сбрасываем указатель на начало аргумента
+          }
+          else if (inp[i + 1] == '\"'){
+            memmove(&inp[i], &inp[i + 2], strlen(&inp[i + 1]) + 1);
+            continue;
           }
           else {
             memmove(&inp[i], &inp[i + 1], strlen(&inp[i + 1]) + 1);
