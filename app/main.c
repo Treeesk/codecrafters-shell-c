b87fc3f81586ffb1058306827ecbbffdf00f1a87
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-13 21:30:54 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -16,15 +16,7 @@ void parse_input(char *inp, char **argv, int *argc, char **outf, short int* err_
     for (int i = 0; inp[i]; i++) {
 
         // Обработка перенаправления вывода
-        if (inp[i] == '1' && inp[i + 1] == '>' && inp[i + 2] == '>'){
-          inp[i] = '\0';
-          *outf = &inp[i + 3];
-          while (**outf == ' ') {
-              (*outf)++;
-          }
-          break;
-        }
-        else if (((inp[i] == '1' && inp[i + 1] == '>') || (inp[i] == '>' && inp[i + 1] == '>')) && !in_quotes) {
+        if ((inp[i] == '1' && inp[i + 1] == '>') && !in_quotes) {
             inp[i] = '\0';
             *outf = &inp[i + 2];
             while (**outf == ' ') {
