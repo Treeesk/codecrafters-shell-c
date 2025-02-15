732fa3e124e675344c7c75843f93dd5e13201613
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-15 21:16:40 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -314,8 +314,8 @@ int main() {
       short int err_f = 0;
       short int appen = 0;
       parse_input(input, argv, &argc, &output_file, &err_f, &appen);
-      char *pth = check_path(argv[0]); // возвращаю полный путь до команды например cat, а затем применяю эту команду к аргументам argv
       printf("%s", input);
+      char *pth = check_path(argv[0]); // возвращаю полный путь до команды например cat, а затем применяю эту команду к аргументам argv
       if (pth != NULL)
         fork_func(pth, argv, output_file, err_f, appen); 
       else {
