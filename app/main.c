9d7892c022ac5b6b97fe4f78f793427340803252
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-16 23:19:41 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -64,7 +64,7 @@ void parse_input(char *inp, char **argv, int *argc, char **outf, short int* err_
       // Обработка перенаправления вывода
 
       if (inp[i] == '2' && inp[i + 1] == '>' && inp[i + 2] == '>' && !in_quotes){
-        inp[i] == '\0';
+        inp[i] = '\0';
         *outf = &inp[i + 3];
         *app = 1;
         *err_f = 1;
