fbcb8dceb23a9be62c2297deb5521fa041c430d2
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-17 22:36:14 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -317,16 +317,14 @@ int main() {
         printf("cd: %s: No such file or directory\n", &input[3]);
     }
     else{
-      if (a == 2)
-        printf("%s", input);
       char *argv[10] = { 0 };
       int argc = 0;
       char *output_file = NULL;
       short int err_f = 0;
       short int appen = 0;
       parse_input(input, argv, &argc, &output_file, &err_f, &appen);
-      // if (a == 1)
-      //   printf("%s", argv[0]);
+      if (a == 2)
+        printf("%s", argv[0]);
       char *pth = check_path(argv[0]); // возвращаю полный путь до команды например cat, а затем применяю эту команду к аргументам argv
       // if (a == 2)
       //   printf("%s\n", pth);
