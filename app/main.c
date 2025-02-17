5afdaadaaa74ef763442e01d1c7206c7c4a60854
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-17 23:31:50 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -250,6 +250,8 @@ int main() {
     a++;
     printf("$ "); // Выводим приглашение
     fflush(stdout);
+    if ( a == 2)
+      printf("YE\n");
     while (1) {
       char c = getchar(); // Считываем символ
       if (c == '\t') { // Обработка Tab (автодополнение)
@@ -277,7 +279,7 @@ int main() {
       }
   }
   if (a == 2)
-    printf("!!!");
+    printf("%s", input);
   restore_terminal_mode(&original_settings);
     if (strcmp(input, "exit 0") == 0)
       exit(0);
