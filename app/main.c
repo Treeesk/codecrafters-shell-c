3d4d65a465eb2eb7baf3d6e9b916801b4725fbf2
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-17 22:16:46 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -194,6 +194,7 @@ void fork_func(char *full_path, char **argv, char *outf, short int err_f, short 
       close(fd);
     }
     full_path[strlen(full_path)] = '\0';
+    printf("%s()))))", full_path);
     execv(full_path, argv);
     perror("execv!!!!!!"); // если ошибка в Execv
     exit(1);
