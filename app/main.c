d91193e37d127e1de5280992b2248843ff5d6ff2
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-20 17:48:54 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -263,6 +263,7 @@ int autocomp(char* w) {
               if (strncmp(w, entry->d_name, strlen(w)) == 0) {
                   snprintf(full_path, sizeof(full_path), "%s/%s", dir, entry->d_name);
                   if (access(full_path, F_OK)) {
+                    printf("%s", full_path);
                     for (int i = 0; i < match_cnt; i++){
                       if (strcmp(matches[i], entry->d_name) == 0){
                         break;
