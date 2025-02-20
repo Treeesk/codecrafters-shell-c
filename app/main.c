64be1725bf2f8162934f19ebb6330f63ea8e8ff8
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-20 13:11:43 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -236,7 +236,7 @@ int comp(void* a, void* b){
 }
 
 int autocomp(char* w) {
-  static char matches[100][100]; // массив под дополнения 
+  static char matches[100][10]; // массив под дополнения 
   static int match_cnt = 0; // счетчик для массива с дополнениями
 
   // Проверяем заранее заданные команды
