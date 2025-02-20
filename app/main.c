827e4126420ab651b470336a88b40c38ff27999b
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-21 00:44:58 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -328,6 +328,8 @@ int main() {
               // }
               // else 
               //   ungetc(c, STDIN_FILENO);
+              c = getchar();
+              printf("%c", c);
               input_len = strlen(input);
               printf("\r$ %s ", input); // Перерисовываем строку ввода
               // input[input_len++] = ' ';
