4154683e5995103f4743467be9ba2f4bebe3d4a4
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-21 15:53:18 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -233,7 +233,7 @@ int tab_press_cnt = 0; // счетчик tab-нажатий
 
 int comp(const void* a, const void* b){
   //return strcmp((char*)a, (char*)b);
-  return (int)(strlen(*(char**)a) - strlen(*(char**) b));
+  return (int)(strlen((char*)a) - strlen((char*) b));
 }
 
 // Функция для нахождения наибольшего общего префикса
@@ -323,10 +323,10 @@ int autocomp(char* w) {
       // tab_press_cnt = 0;
       // return 0;
       // Находим наибольший общий префикс
+      qsort(matches, match_cnt, sizeof(matches[0]), comp);
       printf("\n%s", matches[0]);
       printf("234234242");
       fflush(stdout);
-      qsort(matches, match_cnt, sizeof(matches[0]), comp);
       char* prefix = longest_common_prefix(matches, match_cnt);
       if (prefix) {
           strcpy(w, prefix); // Заменяем строку ввода на наибольший общий префикс
