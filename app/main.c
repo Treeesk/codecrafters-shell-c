b7d4b7cabc5ede4bd8c4a2790e6efece1620ae55
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-21 13:48:32 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -258,7 +258,7 @@ int autocomp(char* w) {
     DIR *dp = opendir(dir); // открытие всей директории(поток директории) 
     if (dp != NULL) {
       printf("dsfsdfds");
-      write(STDOUT_FILENO, "43", 2);
+      fflush(stdout);
         struct dirent *entry; // структура для рассмотрения поддиректории или отдельного файла
         while ((entry = readdir(dp)) != NULL) {
               if (strncmp(w, entry->d_name, strlen(w)) == 0) {
