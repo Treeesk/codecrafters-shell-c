ef9cae4d53861d10946e98cc0ab008db24650ba8
Treeesk <yaroslav.kalinin.06@yandex.ru>
2025-02-21 15:38:02 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -324,7 +324,7 @@ int autocomp(char* w) {
       // return 0;
       // Находим наибольший общий префикс
       qsort(matches, match_cnt, sizeof(matches[0]), comp);
-      printf("%s", matches[0]);
+      printf("\n%s", matches[0]);
       fflush(stdout);
       char* prefix = longest_common_prefix(matches, match_cnt);
       if (prefix) {
